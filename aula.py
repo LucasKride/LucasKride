95931a80838c3b44aaf6cbaedd41b1ccd1e6ac6f
LucasKride <102673310+LucasKride@users.noreply.github.com>
2022-03-31 15:04:45 -0300

    Update aula.py

--- aula.py
+++ aula.py
@@ -5,7 +5,7 @@ print("Bem vindo ao jogo da adivinhação")
 print("********************************")
 
 
-numero_secreto = random.randrange(0,101)
+numero_secreto = random.randrange(1,101)
 total_de_tentativas = 3
 
 for rodada in range(1, total_de_tentativas + 1):
