2d3f3d283ccc2da8f3b785e757e803547d669e23
Lucas Ide <lucas.ide0@gmail.com>
2022-04-05 16:06:56 -0300

    novo

--- exercicios.py
+++ exercicios.py
@@ -1,3 +1,19 @@
-palavra = "alura"
-palavra.upper()
-print(palavra) #qual é o resultado?
+print("Qual seu sexo?")
+print("Homem (H) / Mulher (M)")
+
+H = "H"
+M = "M"
+
+sexo = str(input("Digite seu sexo: "))
+
+
+if(sexo == H):
+    altura = float(input("Qual sua altura?: "))
+    peso_ideal = (72.7 * altura) - 58
+    print("Seu peso ideal e {}".format(peso_ideal))
+elif(sexo == M):
+    altura = float(input("Qual sua altura?: "))
+    peso_ideal = (62.1 * altura) - 44.7
+    print("Seu peso ideal e: {}".format(peso_ideal))
+else:
+    print("Escolha um numero valido")
--- forca.py
+++ forca.py
@@ -10,23 +10,33 @@ def jogar():
     print("**************************")
 
     palavra_secreta = "banana"
+    letras_acertadas = ["_","_","_","_","_","_"]
+
     enforcou = False
     acertou  = False
+    erro = 0
+
+    print(letras_acertadas)
 
     while(not enforcou and not acertou):
         
         chute = input("Qual a letra? ")
         chute = chute.strip()
 
-        index = 0
-        for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()):
-                print("Encontrei a letra {} na posicao {}".format(letra,index))
-            index = index + 1
-
-
-        print("Jogando")
-
+        if(chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(chute.upper() == letra.upper()):
+                    letras_acertadas[index] = letra
+                index = index + 1
+        else:
+            erro = erro + 1
+            print("O numero de erros foi {}".format(erro))
+        
+        enforcou = erro == 6
+        print("Enforcou!")
+        print(letras_acertadas)
+        print("jogando...")
 
     print("Fim de Jogo!")
 
